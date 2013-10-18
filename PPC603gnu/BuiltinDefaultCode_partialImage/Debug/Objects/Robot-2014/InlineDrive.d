5cf0d1a4ed79add52cda1e9623f57ccdc004d533
amomchilov <alexandermomchilov@gmail.com>
2013-10-18 01:43:32 -0400

    123

---
+++ PPC603gnu/BuiltinDefaultCode_partialImage/Debug/Objects/Robot-2014/InlineDrive.d
@@ -0,0 +1,5 @@
+BuiltinDefaultCode_partialImage/Debug/Objects/Robot-2014/InlineDrive.o:  \
+ C:/WindRiver/workspace/Robot-2014/InlineDrive.cpp \
+  C:/WindRiver/workspace/Robot-2014/InlineDrive.hpp
+
+C:/WindRiver/workspace/Robot-2014/InlineDrive.hpp:
